2fc66d4a67da203375974b19ef2537df8287eba1
Eva Kozakova <evkozakova@seznam.cz>
2021-06-04 10:01:35 +0200

    add create transaction

--- lib/transaction_list.dart
+++ lib/transaction_list.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:mystocks_ui/crypto_api.dart';
 import 'package:mystocks_ui/model/crypto_transaction_list_entity.dart';
 
+import 'constants/style.dart';
 import 'model/crypto_transaction.dart';
+import 'model/crypto_transaction_create_entity.dart';
 
 class TransactionList extends StatefulWidget {
   final String userId;
@@ -21,10 +23,19 @@ class _TransactionListState extends State<TransactionList> {
   String userId;
   Future<CryptoTransactionListEntity> futureTransactions;
 
+  String formAmountOfBtc = "";
+  String formTransactionValueInCrowns = "";
+  String formAssetType = "";
+  DateTime? formTransactionDate;
+  TextEditingController? dateField = TextEditingController();
+  DateTime selectedDate = DateTime.now();
+
   _TransactionListState({required this.userId, required this.futureTransactions});
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Transaction List"),
@@ -70,9 +81,161 @@ class _TransactionListState extends State<TransactionList> {
               return CircularProgressIndicator();
             },
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton.icon(
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: defaultPadding * 1.5,
+                          vertical: defaultPadding
+                      )
+                  ),
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            content: Stack(
+                              children: <Widget>[
+                                Positioned(
+                                  right: -40.0,
+                                  top: -40.0,
+                                  child: InkResponse(
+                                    onTap: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: CircleAvatar(
+                                      child: Icon(Icons.close),
+                                      backgroundColor: Colors.red,
+                                    ),
+                                  ),
+                                ),
+                                Form(
+                                  key: _formKey,
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: EdgeInsets.all(8.0),
+                                        child: TextFormField(
+                                          decoration: InputDecoration(
+                                            border: UnderlineInputBorder(),
+                                            labelText: 'Type',
+                                          ),
+                                          validator: (value) {
+                                            if (value == null || value.isEmpty) {
+                                              return 'Type';
+                                            }
+                                            formAssetType = value;
+                                            return null;
+                                          },
+                                          initialValue: "btc",
+                                          enabled: false,
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.all(8.0),
+                                        child: TextFormField(
+                                          decoration: InputDecoration(
+                                            border: UnderlineInputBorder(),
+                                            labelText: 'enter amount of btc',
+                                          ),
+                                          validator: (value) {
+                                            if (value == null || value.isEmpty) {
+                                              return 'enter amount of btc';
+                                            }
+                                            formAmountOfBtc = value;
+                                            return null;
+                                          },
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.all(8.0),
+                                        child: TextFormField(
+                                          controller: dateField,
+                                          decoration: InputDecoration(
+                                            border: UnderlineInputBorder(),
+                                            labelText: 'select date',
+                                          ),
+                                          validator: (value) {
+                                            if (value == null || value.isEmpty) {
+                                              return 'enter date';
+                                            }
+                                            setState(() {
+                                              value = selectedDate.toString();
+                                            });
+                                            return null;
+                                          },
+                                          onTap: () => _selectDate(context),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.all(8.0),
+                                        child: TextFormField(
+                                          decoration: InputDecoration(
+                                            border: UnderlineInputBorder(),
+                                            labelText: 'enter transaction value in crowns',
+                                          ),
+                                          validator: (value) {
+                                            if (value == null || value.isEmpty) {
+                                              return 'enter transaction value in crowns';
+                                            }
+                                            formTransactionValueInCrowns = value;
+                                            return null;
+                                          },
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: ElevatedButton(
+                                          child: Text("Submit"),
+                                          onPressed: () {
+                                            if (_formKey.currentState!.validate()) {
+                                              _formKey.currentState!.save();
+                                              CryptoTransactionCreateEntity ctce = new CryptoTransactionCreateEntity(
+                                                  assetType: formAssetType,
+                                                  amount: formAmountOfBtc,
+                                                  transactionDate: formTransactionDate!,
+                                                  transactionValue: formTransactionValueInCrowns);
+                                              CryptoApi().saveCryptoTransaction(ctce, userId);
+
+                                              // save form
+                                            }
+                                          },
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        });
+                  },
+                  icon: Icon(Icons.add),
+                  label: Text("Add new")
+              ),
+            ],
+          )
+
         ],
       ),
     );
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+        formTransactionDate = selectedDate;
+        dateField?.text = "${selectedDate.toLocal()}".split(' ')[0];
+      });
   }
 
 }
